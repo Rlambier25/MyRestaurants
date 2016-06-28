c636e1094c96e13d0bab3256944ef4c828c9411b
Reed Lambier <rlambier46@gmail.com>
2016-06-27 18:55:28 -0700

    will add working data to different pages

--- app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
@@ -1,37 +1,3 @@
-//package com.epicodus.myrestaurants;
-//
-//import android.content.Intent;
-//import android.support.v7.app.AppCompatActivity;
-//import android.os.Bundle;
-//import android.view.View;
-//import android.widget.Button;
-//import android.widget.EditText;
-//import android.util.Log;
-//
-//
-//public class MainActivity extends AppCompatActivity {
-//    private static final String TAG = MainActivity.class.getSimpleName();
-//    private Button mFindRestaurantsButton;
-//    private EditText mLocationEditText;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
-//        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
-//        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View v) {
-//                String location = mLocationEditText.getText().toString();
-//                Log.d(TAG, location);
-//                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//    }
-//}
 
 package com.epicodus.myrestaurants;
 
@@ -48,19 +14,20 @@ public class MainActivity extends AppCompatActivity {
     private Button mFindRestaurantsButton;
     private EditText mLocationEditText;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
-        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
 
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String location = mLocationEditText.getText().toString();
-                Log.d(TAG, location);
                 Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                intent.putExtra("location", location);
                 startActivity(intent);
             }
         });
