2b8ddfed91a54e93b0e6fb2d6c3ec9eac16b6608
Reed Lambier <rlambier46@gmail.com>
2016-07-17 19:42:38 -0700

    will add ItemTouch Helper setup. needs trouble shooting in Drawable and util folder

---
+++ app/src/main/java/com/epicodus/myrestaurants/util/ItemTouchHelperAdapter.java
@@ -0,0 +1,9 @@
+package com.epicodus.myrestaurants.util;
+
+/**
+ * Created by Guest on 7/17/16.
+ */
+public class ItemTouchHelperAdapter {
+    boolean onItemMove(int fromPosition, int toPosition);
+    void onItemDismiss(int position);
+}
---
+++ app/src/main/java/com/epicodus/myrestaurants/util/OnStartDragListener.java
@@ -0,0 +1,10 @@
+package com.epicodus.myrestaurants.util;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Guest on 7/17/16.
+ */
+public class OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
+}
